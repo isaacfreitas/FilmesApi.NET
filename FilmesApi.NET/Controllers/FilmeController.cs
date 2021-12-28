32a72c7f3531b1313eba4e0d9a819d4ce10ebe4f
isaacfreitas <isaacfrancafreitas@outlook.com>
2021-12-28 17:31:38 -0300

    Criando POST

---
+++ FilmesApi.NET/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApi.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.NET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
---
+++ FilmesApi.NET/Models/Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmesApi.NET.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public string Duracao { get; set; } 
+    }   
+}
