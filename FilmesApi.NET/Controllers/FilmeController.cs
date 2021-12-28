fd50ab7b7550abbee46cdceacd0752058e598ddf
isaacfreitas <isaacfrancafreitas@outlook.com>
2021-12-28 19:16:38 -0300

    Método GET + GET by Id

--- FilmesApi.NET/Controllers/FilmeController.cs
+++ FilmesApi.NET/Controllers/FilmeController.cs
@@ -8,12 +8,26 @@ namespace FilmesApi.NET.Controllers
     public class FilmeController
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RecuperaFilmePorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
--- FilmesApi.NET/Models/Filme.cs
+++ FilmesApi.NET/Models/Filme.cs
@@ -1,10 +1,17 @@
-﻿namespace FilmesApi.NET.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.NET.Models
 {
     public class Filme
-    {
+    {   
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O campo Título é obrigatório")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        [StringLength(30,ErrorMessage ="O genero não pode passar de 30 caracteres")]
         public string Diretor { get; set; }
         public string Genero { get; set; }
+        [Range(1,600,ErrorMessage ="A duração deve estar entre 1 e 600 minutos")]
         public string Duracao { get; set; } 
     }   
 }
