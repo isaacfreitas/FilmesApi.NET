6ec82088962c61b851e0f928858ac791f2086c97
isaacfreitas <isaacfrancafreitas@outlook.com>
2021-12-31 14:39:22 -0300

    Ajuste bugs pra ficar igual curso,REad me dando erro

---
+++ FilmesApi.NET/Data/FilmeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FilmesApi.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FilmesApi.NET.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
---
+++ FilmesApi.NET/README.md
@@ -0,0 +1,3 @@
+﻿# **API .NET de filmes genérica**
+
+## **README EM CONSTRUÇÃO
